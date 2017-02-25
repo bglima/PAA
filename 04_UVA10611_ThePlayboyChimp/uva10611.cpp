567f46790c9d02fe3e1edd4d7aadba349e8f2830
Bruno Lima <bg.lima@hotmail.com>
2017-02-25 14:16:56 -0300

    UVA10611 The Playboy Chimp submission accepted

--- 04_UVA10611_ThePlayboyChimp/uva10611.cpp
+++ 04_UVA10611_ThePlayboyChimp/uva10611.cpp
@@ -2,52 +2,42 @@
 
 using namespace std;
 
-struct heights{
-	int shorter;
-	int taller;
-};
+int shorter, taller;
 
 int n, q;
 int luchuHeight;
 
+// Iterative binary search. Ends when left > right.
+// Initalize shorter and taller as -1, suposing that they does not exist.
+void divideAndConquer(int chimpHeight[], int left, int right) {
+	shorter = taller = -1;
+  	while (left <= right) {			   // If luchuHeight exists in array, we need to find a shorter and a greater element.
+            int mid = (left + right) / 2;          // Take the middle from sorted array
+            if (chimpHeight[mid] == luchuHeight) { // If there is an equal element
+		shorter = taller = mid;	           // Start from it
+		while ( chimpHeight[--shorter] == luchuHeight ); // And goes back until we find an element that is shorter
+		if( shorter < 0 ) shorter = -1;    // If there isn't one, put -1 back
 
-// Look for a chimp with same height as Luchu. 
-// If there exists one, return the previous and the next chimp heights.
-// Otherwise, if there is not, there are thre possibilities:
-//    1 - Search ends at first position. There is on
-//    2 - Search ends at last position
-//    3 - Search ends at middle
-heights divideAndConquer(int chimpHeight[], int left, int right) {
-	heights pretendents;
-	if( left == right ) {
-		if ( chimpHeight[left] == luchuHeight ) {
-			pretendents.shorter = (left == 0) ? (-1) : (--left);
-			pretendents.taller = (right == n) ? (-1) : (++right);
-			return pretendents;
-		} else if ( luchuHeight > chimpHeight[left] ) {
-			pretendents.shorter = chimpHeight[left];
-			if ( ++right < n && chimpHeight[right] > luchuHeight ) {
-				pretendents.taller = chimpHeight[right];
-			} else pretendents.taller = -1;	
-			return pretendents;
-		} else if ( luchuHeight < chimpHeight[left] ) {
-			pretendents.taller = chimpHeight[left];
-			if ( --left >= 0 && chimpHeight[left] < luchuHeight ) {
-				pretendents.shorter = chimpHeight[left];
-			} else pretendents.shorter = -1;	
-			return pretendents;
-		}
-	}
-	int mid = left + right / 2;
-	if ( chimpHeight[mid] > luchuHeight )  
-		return divideAndConquer( chimpHeight, left, mid);
-	if ( chimpHeight[mid] < luchuHeight )
-		return divideAndConquer( chimpHeight, mid, right);
 
-	// Else...
-	pretendents.shorter = (mid - 1 >= 0) ? (mid - 1) : (-1);
-	pretendents.taller =  (mid + 1 < n) ? (mid + 1) : (-1);
-	return pretendents;
+		while ( chimpHeight[++taller] == luchuHeight ); //  Do the same to find an element that is greater
+		if( taller > n-1) taller = -1;	   // It there isn't one, put -1 back
+		return;
+	    }
+	    else if (chimpHeight[mid] > luchuHeight) { // Otherwise, compare to middle. If middle element is greater...
+		taller = mid;
+                right = mid - 1;
+	    }
+            else {					// If middle element is shorter...
+		shorter = mid;
+                left = mid + 1;
+	    }
+      }
+}
+
+// Formatting output
+void showResults(int chimpHeight[]) {
+     if ( shorter == -1 ) cout << "X "; else cout << chimpHeight[shorter] << " ";
+     if ( taller == -1) cout << "X" << endl; else cout << chimpHeight[taller] << endl;
 }
 
 
@@ -61,7 +51,7 @@ int main() {
 	cin >> q;
 	for( int i = 0; i < q; ++i ) {
 		cin >> luchuHeight;
-		heights pretendents;
-		cout << pretendents.shorter << " " << pretendents.taller << endl;
+		divideAndConquer(chimpHeight, 0, n-1); // Divide and conquer
+		showResults(chimpHeight);	       // Show results
 	}
 }
