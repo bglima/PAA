b76d46b9e8c23b695c46b9197b0eda4c8d97a239
Bruno Lima <bg.lima@hotmail.com>
2017-04-08 20:12:56 -0300

    uva10305 accepted.

--- 14_UVA10305/uva10305.cpp
+++ 14_UVA10305/uva10305.cpp
@@ -6,19 +6,20 @@ int n, m;
 
 vector < vector < int > > adj_list;
 vector < bool > visited;
+vector < int > order;
 vector < int > in_degree;
 
 void init( int size ) {
 	adj_list.clear();
 	in_degree.clear();
     visited.clear();
-
-
+	order.clear();
+	
 	adj_list.resize( size + 1 );
 	in_degree.resize( size + 1 );
     visited.resize( size + 1 );
 
-    for( int i = 0; i <= n; ++i ) {
+    for( int i = 0; i <= m; ++i ) {
         visited[i] = false;
         in_degree[i] = 0;
     }
@@ -30,14 +31,19 @@ void add_edge(int from, int to ) {
 }
 
 void dfs( int current ) { 
+	if ( visited[current] == true )
+		return;
+	
     visited[ current ] = true;
-    cout << "Passed visited check at " << current << endl;
+		
     for( int i = 0; i < adj_list[current].size(); ++i ) {
         int adjacent = adj_list[current][i];
-        cout << " Adjacent: " << adjacent<< endl;
+
         if ( !visited[adjacent] )
             dfs( adjacent );
-        }
+    }
+	order.push_back( current );	
+	
 }
 
 int main() {
@@ -46,15 +52,20 @@ int main() {
 		cin >> m >> n;
 		if (  m == 0 && n == 0 )
 			break;
-		init ( n );
-		cout << "Init ok" << endl;
+		init ( m );
 		for ( int i = 0; i < n; ++i ) {
 			cin >> from >> to;
 			add_edge( from, to );
 		}   
-		cout << "Edges added" << endl;
 		
-        dfs(1);
+		for( int i = 1; i <= m; ++i)
+			if ( in_degree[i] == 0 ) dfs(i);
+		
+		while( !order.empty() ) {
+			cout << order.back() << " ";	
+			order.pop_back();
+		}
+		cout << endl;
 	}
 	return 0;
 }
